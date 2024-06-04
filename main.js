8a062d9a8dedc2fcb212f0ce594c3808c84a94cf
dinaaprilia <132429093+dinaaprilia@users.noreply.github.com>
2024-06-04 15:56:54 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-const createWindow = () => {
-    const win = new BrowserWindow ({
-        title: 'Pengaduan Masyarakat',
-        width: 500,
-        height: 600
-    });
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
 
-    win.loadFile('index.html');
+  mainWindow.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
+  }
 });
